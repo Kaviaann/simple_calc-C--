4fd9374895a841f9c516a58d124d6ec0c6d8f415
Ludwigyelstein <fayiedolshop@gmail.com>
2023-09-18 23:13:44 +0700

    new_Commit

--- code.cpp
+++ code.cpp
@@ -1,59 +1,103 @@
 #include <iostream>
-#include <stdlib.h> //exit
+#include <cstdlib> // For the system function
+#include <thread>  // For the sleep_for function
+using namespace std;
 
-//variable
-int answer; //line 14;2
-char go_answer;
+// Function prototypes
+void calculate(char operation);
+void clearScreen();
 
-int main(){
-
-    using namespace std;
-
+int main() {
     cout << "\n--------------------------------------------\n\n   Welcome to simple calculator! \n     Made by : Ayi \n\n";
 
+    int answer;
+
     cout << "Choose : \n";
-    cout << "   1. Plus\n   2. Minus\n   3. Divide\n   4. Multiplication\n      Answer? :";
+    cout << "   1. Plus\n   2. Minus\n   3. Divide\n   4. Multiplication\n      Answer? : ";
     cin >> answer;
 
-    //go
-    switch (answer)
-    {
-    case 1:
-        //_Plus();
-        break;
-    
-    case 2:
-       // _Minus();
-        break;
+    switch (answer) {
+        case 1:
+            calculate('+');
+            break;
 
-    case 3:
-        //_Divide();
-        break;
-    
-    case 4:
-        //_Multi();
-        break;
+        case 2:
+            calculate('-');
+            break;
 
-    default:
-        cout << "Exit? (y/n) : ";
-        cin >> go_answer;
+        case 3:
+            calculate('/');
+            break;
 
-        if(go_answer == 'y'){
-            cout << endl;
-            cout << endl;
-            exit;
-        }else {
+        case 4:
+            calculate('*');
+            break;
 
-        };
-
-
-
-    };
-    
+        default:
+            cout << "Invalid choice. Exiting.\n";
+            break;
+    }
 
     return 0;
+}
+
+// Function to perform calculation and display result
+void calculate(char operation) {
+    int x, y;
+    cout << "\n\nPlease Enter 2 Values (Must Be Full Integers) : " << endl;
+    
+    cout << "Value 1 : ";
+    cin >> x;
+    cout << endl;
+
+    cout << "Value 2 : ";
+    cin >> y;
+    cout << endl;
+
+    int result;
+    switch (operation) {
+        case '+':
+            result = x + y;
+            break;
+
+        case '-':
+            result = x - y;
+            break;
+
+        case '/':
+            if (y != 0) {
+                result = x / y;
+            } else {
+                cout << "Error: Division by zero is not allowed.\n";
+                return;
+            }
+            break;
+
+        case '*':
+            result = x * y;
+            break;
+
+        default:
+            cout << "Invalid operation.\n";
+            return;
+    }
+
+    cout << "Answer: " << result << "\n";
+
+    cout << "\n\t* Exitting In 2.5s... *";
+    // Menjalankan program selama 2,5 detik (2500 milidetik)
+    this_thread::sleep_for(chrono::milliseconds(2500));
+
+    clearScreen();
+
 
 }
 
+// Function to clear the screen
+void clearScreen() {
+    int clearStatus = system("clear");
 
-//function
+    if (clearStatus != 0) {
+        clearStatus = system("cls");
+    }
+}
